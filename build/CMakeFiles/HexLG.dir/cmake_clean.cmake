d7eebddd87bf4c1933f9e3c9709a52e4bc105b62
delgadoandrea <1zd@redchile.ornl.gov>
2019-03-21 16:40:28 -0400

    First commit

---
+++ build/CMakeFiles/HexLG.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HexLG.dir/HexLG.cc.o"
+  "bin/HexLG.pdb"
+  "bin/HexLG"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HexLG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
